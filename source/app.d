4c3e7aaf8bb07fb8848d60f7839452587770cd6e
Bagomot <perezzloy@gmail.com>
2021-07-25 15:45:49 +0300

    Add test code for deserialization

--- source/app.d
+++ source/app.d
@@ -1,6 +1,31 @@
 import std.stdio;
+import asdf;
+import entities;
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+	string json = readFileAsString("files/1.16.5.json");
+	Root data = json.deserialize!Root;
+
+    JvmArgument[] jvmArgs = data.arguments.jvm;
+
+    writeln(jvmArgs.length);
+}
+
+string readFileAsString(string path)
+{
+    string line;
+
+	import std.file;
+    import std.exception;
+
+    try {
+        auto file = File(path, "r");
+        line = readText(path);
+    }
+    catch (ErrnoException ex) {
+        ex.writeln;
+    }
+
+    return line;
 }
